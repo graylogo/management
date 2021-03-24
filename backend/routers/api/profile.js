eb3b3f96b16615ca6b6327580a315a1a457038f8
graylogo <graylogo@gmail.com>
2021-03-24 17:25:48 +0800

    编辑和删除接口

--- backend/routers/api/profile.js
+++ backend/routers/api/profile.js
@@ -27,6 +27,7 @@ router.post('/add',passport.authenticate('jwt',{session:false}),(req,res)=>{
         res.json(profile)
     })
 })
+
 // $route post api/profile/
 // @desc  查询所有信息
 // access  Private
@@ -51,5 +52,40 @@ router.get('/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
         res.json(profile)
     }).catch(err=>res.status(400).json(err))
  })
+
+// $route post api/profile/edit
+// @desc  修改资金接口
+// access  Private
+router.post('/edit/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    const profileDetail = {}
+    if(req.body.type) profileDetail.type = req.body.type
+    if(req.body.describe) profileDetail.describe = req.body.describe
+    if(req.body.income) profileDetail.income = req.body.income
+    if(req.body.expend) profileDetail.expend = req.body.expend
+    if(req.body.cash) profileDetail.cash = req.body.cash
+    if(req.body.remark) profileDetail.remark = req.body.remark
+    Profile.findOneAndUpdate(
+        {_id: req.params.id},
+        {$set: profileDetail},
+        {new: true}
+    ).then(profile=>{
+        res.json(profile)
+    })
+})
+
+// $route post api/profile/delete
+// @desc  删除资金接口
+// access  Private
+router.delete('/delete/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    Profile.findOneAndRemove(
+        {_id: req.params.id},
+        {useFindAndModify: false}
+    ).then(profile=>{
+        if(!profile){
+            return res.status(400).json('不存在的数据')
+        }
+        return res.json(profile)
+    }).catch(err=>res.status(400).json('删除失败！'))
+})
 
 module.exports = router
