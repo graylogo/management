549cdc74f7d747b3ecd2130791fc141cc161eb35
graylogo <graylogo@gmail.com>
2021-03-26 10:45:51 +0800

    前端初始化完成

---
+++ frontend/src/views/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+        index
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Index',
+components: {},
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/vue.config.js
@@ -0,0 +1,8 @@
+// vue.config.js
+
+module.exports = {
+    devServer: {
+      // 这里配置端口号
+      port: 3000
+    }
+  }
